5c194e9e3555d76c6e2f69af274d38de19f32d06
YIMHYEONGSUP <101487300+YIMHYEONGSUP@users.noreply.github.com>
2022-09-11 14:40:00 +0900

    2022 09 11 redis / swagger
    setting redis , swagger configuration

--- backend/src/main/java/hyeong/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/hyeong/backend/domain/member/controller/MemberController.java
@@ -6,6 +6,9 @@ import hyeong.backend.domain.member.dto.MemberJoinResponseDTO;
 import hyeong.backend.domain.member.dto.MemberResponseDTO;
 import hyeong.backend.domain.member.entity.persist.Member;
 import hyeong.backend.domain.member.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.net.URI;
 
+@Api("회원 관리 Api")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -26,7 +30,10 @@ public class MemberController {
 
     private final MemberService memberService;
 
+
     @PostMapping("/join")
+    @ApiParam(name = "회원 가입 데이터 전달 DTO")
+    @ApiOperation(value = "회원 가입", notes = "회원 정보를 입력받아 저장한다.")
     public ResponseEntity<MemberJoinResponseDTO> join(@Valid @RequestBody MemberJoinRequestDTO requestDTO) {
 
         log.info("controller request = {}" , requestDTO.getEmail());
@@ -36,4 +43,6 @@ public class MemberController {
         URI createdURI = URI.create(String.format("/api/v1/join", member.getId()));
         return ResponseEntity.created(createdURI).body(memberService.save(member));
     }
+
+    
 }
